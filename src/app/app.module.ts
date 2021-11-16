0c2fe50710344775d1997a7552006b05f67d4dca
Chovi12345 <mi.rodriguez@newhorizons.edu.do>
2021-11-16 18:05:45 +0000

    Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { AppComponenttableComponent } from './app-componenttable/app-componenttable.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    AppComponenttableComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
